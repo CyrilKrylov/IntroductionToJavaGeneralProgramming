567b9b0d916e47d42e8c2d5015f2d1064a3a27d8
MartIden <fallenbraker@gmail.com>
2020-04-21 15:20:59 +0300

    Initial commit

---
+++ Линейные программы/src/LinearProgramsApp.java
@@ -0,0 +1,7 @@
+/*Basics of software code development*/
+
+public class LinearProgramsApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Линейные программы/src/linear_programs/Task1.java
@@ -0,0 +1,9 @@
+package linear_programs;
+
+public class Task1 {
+
+    public static double getZ(double a, double b, double c) {
+        return (a - 3.0);
+    }
+
+}
